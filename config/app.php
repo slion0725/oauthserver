e0869da0258c7044f0bc5051f5aa2b7248e4e82e
Miles Wu <mileswu@fashionguide.com.tw>
2017-02-08 13:20:42 +0800

    Passport Service Providers

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Passport Service Providers...
+        Laravel\Passport\PassportServiceProvider::class,
+
     ],
 
     /*
