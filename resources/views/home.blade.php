aa02c7b1767bcc3af1f054af671b092c8d412ca3
Miles Wu <mileswu@fashionguide.com.tw>
2017-02-08 15:03:49 +0800

    app.js加入vue component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <passport-clients></passport-clients>
-                <passport-authorized-clients></passport-authorized-clients>
-                <passport-personal-access-tokens></passport-personal-access-tokens>
-            </div>
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
     </div>
 </div>
