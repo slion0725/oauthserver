2359eda58c09072c58a96c68a207f44ee1e8f9e8
Miles Wu <mileswu@fashionguide.com.tw>
2017-02-09 13:36:18 +0800

    認證範圍

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,5 +56,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // Scope
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
     ];
 }
